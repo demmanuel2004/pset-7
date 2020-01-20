8f99513a1518af351b3906f8cb6c90ecd6b62f90
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2020-01-20 15:14:16 -0500

    new commit

--- src/exercises.js
+++ src/exercises.js
@@ -69,15 +69,100 @@ function difference(numbers) {
 }
 
 function max(number) {
-  // write your code here
+  if (!number || number.length === 0) {
+return undefined;
+}
+
+var length = number.length;
+
+if ( length < 3 ) {
+return undefined;
+}
+
+if( length % 2 == 0 ) {
+return undefined;
+}
+
+var middle = parseInt( length / 2 ); 
+
+var largest = number[0]; 
+
+for (i = 0; i < length; i++) {
+
+if (isNaN(number[i])) {
+return undefined;
+}
+
+if( i == 0 || i == middle || i == (length - 1) ) { 
+
+if( largest < number[i] ) { 
+largest = number[i]; 
+}
+}
+}
+
+return largest;
+}
 }
 
 function middle(values) {
-  // write your code here
+  if (!values || values.length === 0) {
+return [];
+}
+
+var length = values.length;
+
+if ( length < 3 ) {
+return [];
+}
+
+if( length % 2 == 0 ) {
+return [];
+}
+
+var middle = parseInt( length / 2 ) ; 
+let arr = [];
+
+
+for (i = 0; i < length; i++) {
+if( i == (middle-1) || i == middle ) { 
+arr.push ( values[i] );
+}
+}
+
+return arr;
+
+}
+
 }
 
 function increasing(numbers) {
-  // write your code here
+  if (!numbers || numbers.length === 0) {
+return undefined;
+}
+
+var length = numbers.length;
+
+if ( length < 3 ) {
+return undefined;
+}
+
+
+for (i = 0; i < length; i++) { 
+if (isNaN(numbers[i])) {
+return ;
+}
+}
+
+for( i = 0; i < numbers.length - 2; i++) {
+if( ( numbers[i+1] - numbers[i] == 1 ) && ( numbers[i+2] - numbers[i+1] == 1 ) ) {
+
+return true;
+}
+}
+
+return false;
+}
 }
 
 function everywhere(values, x) {
